74ffb5313e00873a2d0145ce3631f5a7448e2c56
Deven-ai12 <mankardevendra33@gmail.com>
2026-09-02 12:07:02 +0530

    Fix portfolio API URL and authentication

--- src/services/portfolioService.js
+++ src/services/portfolioService.js
@@ -4,7 +4,7 @@ const portfolioService = {
 
     getPortfolio: async () => {
 
-        const response = await api.get("/api/portfolio");
+        const response = await api.get("/portfolio");
 
         return response.data;
     }
@@ -14,30 +14,3 @@ const portfolioService = {
 export default portfolioService;
 
 
-// import axios from "axios";
-
-// const API_URL = "http://localhost:8080/api/portfolio";
-
-// const getConfig = () => {
-//   const token = localStorage.getItem("token");
-
-//   return {
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//       "Content-Type": "application/json",
-//     },
-//   };
-// };
-
-// const getPortfolio = async () => {
-//   const response = await axios.get(
-//     API_URL,
-//     getConfig()
-//   );
-
-//   return response.data;
-// };
-
-// export default {
-//   getPortfolio,
-// };
