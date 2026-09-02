c3144580ff715fa77f62b16a423b476f460e673b
Deven-ai12 <mankardevendra33@gmail.com>
2026-09-02 12:00:30 +0530

    Fix wallet API URL and authentication

--- src/services/walletService.js
+++ src/services/walletService.js
@@ -2,7 +2,7 @@ import api from "../api/axios";
 
 const getWallet = async () => {
 
-    const response = await api.get("/api/wallet");
+    const response = await api.get("/wallet");
 
     return response.data;
 };
@@ -10,7 +10,7 @@ const getWallet = async () => {
 const deposit = async (amount) => {
 
     const response = await api.post(
-        "/api/wallet/deposit",
+        "/wallet/deposit",
         {
             amount: Number(amount),
         }
@@ -37,57 +37,3 @@ export default {
     withdraw,
 };
 
-
-// import axios from "axios";
-
-// const API_URL = "http://localhost:8080/api/wallet";
-
-// const getConfig = () => {
-//     const token = localStorage.getItem("token");
-
-//     return {
-//         headers: {
-//             Authorization: `Bearer ${token}`,
-//             "Content-Type": "application/json",
-//         },
-//     };
-// };
-
-// const getWallet = async () => {
-//     const response = await axios.get(
-//         API_URL,
-//         getConfig()
-//     );
-
-//     return response.data;
-// };
-
-// const deposit = async (amount) => {
-//     const response = await axios.post(
-//         `${API_URL}/deposit`,
-//         {
-//             amount: Number(amount),
-//         },
-//         getConfig()
-//     );
-
-//     return response.data;
-// };
-
-// const withdraw = async (amount) => {
-//     const response = await axios.post(
-//         `${API_URL}/withdraw`,
-//         {
-//             amount: Number(amount),
-//         },
-//         getConfig()
-//     );
-
-//     return response.data;
-// };
-
-// export default {
-//     getWallet,
-//     deposit,
-//     withdraw,
-// };
